bfbba9f44bba0000116723be4bbd79ca34cf1346
Ashish Shukla <akshkgd@gmail.com>
2022-06-16 21:04:44 +0530

    create element JS

--- DOM/students.js
+++ DOM/students.js
@@ -4,13 +4,42 @@ function addStudent(){
     let student = {}
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
-    let batch = document.getElementById('batch').value;
 
     student.name = name;
     student.email = email;
-    student.batch = batch;
 
     students.push(student)
-    console.log(students)
+    // creating new element
+
+    document.getElementById('studentsParentContainer').innerHTML = '';
+    document.getElementById('name').value ='';
+    document.getElementById('email').value = '';
+
+
+    students.map((student)=>{
+        let parentDiv = document.getElementById('studentsParentContainer');
+        //creating new element
+        let studentDiv = document.createElement('div');
+        let studentName = document.createElement('h3');
+        let studentEmail = document.createElement('p');
+
+        //adding class name
+        studentDiv.classList.add('student');
+        studentName.classList.add('student-name');
+        studentEmail.classList.add('student-email');
+
+
+        //adding values
+        studentName.innerText = student.name;
+        studentEmail.innerText = student.email;
+
+        // appending the element
+        parentDiv.appendChild(studentDiv);
+        studentDiv.appendChild(studentName);
+        studentName.appendChild(studentEmail);
+    })
+
+
+
 
 }
