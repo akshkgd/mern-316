eacfd2863012da80bee59281ef2ced80642c80c0
Ashish Shukla <akshkgd@gmail.com>
2022-07-21 22:58:16 +0530

    react

---
+++ first-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Demo from  './components/Demo'
+function App() {
+  let user = {
+    name: 'ahsish',
+    email: 'ahsish@gmail.cpm'
+  }
+  return (
+    <div>
+      <h1>Your name is:  {name} </h1>
+     
+
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ first-app/src/components/Demo.js
@@ -0,0 +1,14 @@
+function Demo(){
+    return(
+        <div>
+            <h1>this is my demo component</h1>
+            <p>this is a para of demo compoent</p>
+        </div>
+    )
+}
+
+export default Demo;
+
+
+
+// function name(){}
